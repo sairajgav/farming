dbe3ed8fe5c93a7f1a15efa6b1aed63228d74bb6
sairajgav <88610714+sairajgav@users.noreply.github.com>
2021-08-08 13:14:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function add(){
+var result=
+Number(document.getElementById("one").value)+
+Number(document.getElementById("two").value);
+window.alert(result);
+}
+function subraction(){
+var result=
+Number(document.getElementById("one").value)-
+Number(document.getElementById("two").value);
+window.alert(result);
+}
